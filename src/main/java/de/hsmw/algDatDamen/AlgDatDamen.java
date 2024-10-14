b5e7a51962fdaa6bafff8c6b5e2d4f1308e711b3
Lucas Seidel <lseidel2@hs-mittweida.de>
2024-10-14 23:18:29 +0200

    IntelliJ Projekt

---
+++ src/main/java/de/hsmw/algDatDamen/AlgDatDamen.java
@@ -0,0 +1,17 @@
+package de.hsmw.algDatDamen;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AlgDatDamen extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("Moin!");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
