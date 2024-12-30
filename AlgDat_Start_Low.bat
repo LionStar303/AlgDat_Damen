1f9be8e0b74175cf5d9a5d91554c8bc8743203d8
phoenix2557 <ben_harms1@web.de>
2024-12-31 00:42:09 +0100

    Changed Low_Mode

--- AlgDat_Start_Low.bat
+++ AlgDat_Start_Low.bat
@@ -2,6 +2,6 @@
 cd /d "%~dp0"  REM Change to the directory of the batch file
 start "" .\run_build_server.bat
 cd /d "%~dp0Minecraft"  
-.\portablemc.exe --work-dir .\ --main-dir .\ -s localhost
+.\portablemc.exe --work-dir .\ --main-dir .\ start -s localhost
 
 pause
