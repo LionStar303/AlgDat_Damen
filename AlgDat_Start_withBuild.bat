389f102b933fb977a1e0aaebd84bb4f4dfb6188a
M-0-K <62558735+M-0-K@users.noreply.github.com>
2024-12-31 10:12:22 +0100

    batches for release

--- AlgDat_Start_withBuild.bat
+++ AlgDat_Start_withBuild.bat
@@ -1,8 +1,6 @@
 @echo off
 cd /d "%~dp0"  REM Change to the directory of the batch file
-start "" .\Server\startServer.bat
-
-
+start "" .\run_build_server.bat
 cd /d "%~dp0Minecraft"  
 .\portablemc.exe --work-dir .\ --main-dir .\ start fabric:1.21.1 -s localhost
 
